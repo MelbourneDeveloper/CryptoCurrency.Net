f04c12835517bb8668aa81493e519e40f1b0ffc4
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-15 11:53:07 +1100

    Start adding transactions

---
+++ src/CryptoCurrency.Net/Model/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CryptoCurrency.Net.Model
+{
+    public class Transaction
+    {
+        public List<TransactionPiece> Inputs { get; } = new List<TransactionPiece>();
+        public List<TransactionPiece> Outputs { get; } = new List<TransactionPiece>();
+
+        public decimal Value => Outputs.Sum(o => o.Amount);
+    }
+}
---
+++ src/CryptoCurrency.Net/Model/TransactionPiece.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoCurrency.Net.Model
+{
+    public class TransactionPiece
+    {
+        public string Address { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
