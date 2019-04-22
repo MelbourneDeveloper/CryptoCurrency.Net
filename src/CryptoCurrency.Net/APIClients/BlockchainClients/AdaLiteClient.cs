6af82f5f4c7dee309203f05c8f1d3cf19277dbe9
Christian <anemail@nothing.com>
2019-04-22 14:00:09 +1000

    Ada redundancy - check!

--- src/CryptoCurrency.Net/APIClients/BlockchainClients/AdaLiteClient.cs
+++ src/CryptoCurrency.Net/APIClients/BlockchainClients/AdaLiteClient.cs
@@ -6,18 +6,19 @@ using System;
 namespace CryptoCurrency.Net.APIClients
 {
     /// <summary>
-    /// https://cardanodocs.com/technical/explorer/api/
+    /// http://explorer2.adalite.io
     /// </summary>
-    public class CardanoExplorerClient : CardanoExplorerBase, IBlockchainClient
+    public class AdaLiteClient : CardanoExplorerBase, IBlockchainClient
     {
+
         #region Public Static Fields
         public static CurrencyCapabilityCollection CurrencyCapabilities { get; } = new CurrencyCapabilityCollection { CurrencySymbol.Cardano };
         #endregion
 
-        public CardanoExplorerClient(CurrencySymbol currency, IRestClientFactory restClientFactory) : base(currency, restClientFactory)
+        public AdaLiteClient(CurrencySymbol currency, IRestClientFactory restClientFactory) : base(currency, restClientFactory)
         {
         }
 
-        public override Uri BaseAddress => new Uri("https://cardanoexplorer.com");
+        public override Uri BaseAddress => new Uri("http://explorer2.adalite.io");
     }
 }
