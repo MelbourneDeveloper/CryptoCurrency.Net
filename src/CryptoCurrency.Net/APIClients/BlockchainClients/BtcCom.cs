36e95f27bf2029c61b248571fdca355f260d9551
Christian Findlay <a@a.com>
2019-11-16 11:30:54 +1100

    Start filling out the client class

---
+++ src/CryptoCurrency.Net/APIClients/BlockchainClients/BtcCom.cs
@@ -0,0 +1,35 @@
+﻿using CryptoCurrency.Net.APIClients.BlockchainClients;
+using CryptoCurrency.Net.APIClients.BlockchainClients.CallArguments;
+using CryptoCurrency.Net.Model;
+using RestClientDotNet;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.Net.APIClients
+{
+    public class BtcCom : BlockchainClientBase, IBlockchainClient
+    {
+        #region Constructor
+        public BtcCom(CurrencySymbol currency, IRestClientFactory restClientFactory) : base(currency, restClientFactory)
+        {
+        }
+        #endregion
+
+        #region Private Static Fields
+        public static CurrencyCapabilityCollection CurrencyCapabilities { get; } = new CurrencyCapabilityCollection { CurrencySymbol.BitcoinCash };
+        #endregion
+
+        #region Overrides
+        protected override Func<GetAddressesArgs, Task<IEnumerable<BlockChainAddressInformation>>> GetAddressesFunc { get; } = async getAddressesArgs =>
+        {
+            return null;
+        };
+
+        public override Task<BlockChainAddressInformation> GetAddress(string address)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
