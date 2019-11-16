44104fdd76cdae6e895369b61b185ab4bb7a152d
Christian Findlay <a@a.com>
2019-11-16 11:09:34 +1100

    Add the BCH address converter

---
+++ src/CryptoCurrency.Net/BCH/CashAddrConversionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CryptoCurrency.Net.BCH
+{
+    public class CashAddrConversionException : Exception
+    {
+        public CashAddrConversionException()
+            : base()
+        {
+        }
+        public CashAddrConversionException(string message)
+            : base(message)
+        {
+        }
+    }
+}
